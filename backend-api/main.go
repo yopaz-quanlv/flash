55c80629f22281225c6767d065ae0865d282fd6c
yopaz-quanlv <quanlv@yopaz.vn>
2025-07-09 14:45:55 +0700

    Add simple CORS middleware (#2)

--- backend-api/main.go
+++ backend-api/main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/joho/godotenv"
-    "github.com/gin-gonic/gin"
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
-    "github.com/example/cms/backend/models"
-    "github.com/example/cms/backend/routes"
+	"github.com/example/cms/backend/models"
+	"github.com/example/cms/backend/routes"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func main() {
-    godotenv.Load()
-    dsn := os.Getenv("DB_DSN")
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal(err)
-    }
-    db.AutoMigrate(&models.Post{})
+	godotenv.Load()
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(&models.Post{})
 
-    r := gin.Default()
-    routes.RegisterRoutes(r, db)
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+	routes.RegisterRoutes(r, db)
 
-    r.Run(":8080")
+	r.Run(":8080")
 }
